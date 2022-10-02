e72c189f52f79d86d22a6c93fa65e777a76ff6ba
Mohammad Umar <60454035+mo-umar@users.noreply.github.com>
2022-10-02 01:47:38 -0400

    db connection to cloud

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,11 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","swe4633_grade_average");
+$servername = "DB_HOST";
+$username = "DB_USERNAME";
+$password = "DB_PASSWORD";
+$dbname = "DB_DATABASE";
+
+$con = mysqli_connect($servername, $username, $password, $dbname);
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
