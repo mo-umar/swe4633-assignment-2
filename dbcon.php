82f9b9c413db383841ce0fbeecfdafbf9848c47c
Mohammad Umar <umarmohammad582@gmail.com>
2022-10-02 20:15:31 -0400

    Added index file and composer.json

--- dbcon.php
+++ dbcon.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","swe4633_grade_average");
+$con = mysqli_connect("swe4633firstdb.c7ewahotbzfq.us-east-2.rds.amazonaws.com","admin","1234567890","swe4633firstdb");
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
