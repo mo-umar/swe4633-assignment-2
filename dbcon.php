c5b095adad1cbaef74f0d4fe7a316915a4eb4046
Mohammad Umar <umarmohammad582@gmail.com>
2022-10-02 21:33:10 -0400

    db connection

--- dbcon.php
+++ dbcon.php
@@ -1,9 +1,20 @@
-<?php
 
-$con = mysqli_connect("swe4633firstdb.c7ewahotbzfq.us-east-2.rds.amazonaws.com","admin","1234567890","swe4633firstdb");
+<?php
+/* Development Connection
+$con = mysqli_connect("localhost","root","","swe4633_grade_average");
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
 }
+*/
+
+// Remote Connection
+$host = 'us-cdbr-east-06.cleardb.net';
+$user = 'b6792ab368ddb3';
+$pass = 'bda03ad9';
+$db = 'heroku_c43a872891278a3';
+$charset = 'utf8mb4';
+
+$con = mysqli_connect($host,$user,$pass,$db,$charset);
 
 ?>
