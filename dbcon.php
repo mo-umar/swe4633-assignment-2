e4a3e7e1f5ac0ebdc32abcc31c1a3b4d94ee2644
Mohammad Umar <60454035+mo-umar@users.noreply.github.com>
2022-10-02 21:45:44 -0400

    con

--- dbcon.php
+++ dbcon.php
@@ -8,12 +8,11 @@ if(!$con){
 */
 
 // Remote Connection
-$host = 'us-cdbr-east-06.cleardb.net';
-$user = 'b6792ab368ddb3';
-$pass = 'bda03ad9';
-$db = 'heroku_c43a872891278a3';
-$charset = 'utf8mb4';
 
-$con = mysqli_connect($host,$user,$pass,$db,$charset);
+$con = mysqli_connect("us-cdbr-east-06.cleardb.net","b6792ab368ddb3","bda03ad9","heroku_c43a872891278a3");
+
+if(!$con){
+    die('Connection Failed'. mysqli_connect_error());
+}
 
 ?>
