8ebba289358e39bd37197c8142068b0f371a31fc
Mohammad Umar <60454035+mo-umar@users.noreply.github.com>
2022-10-03 17:24:54 -0400

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,15 +1,6 @@
 <?php
-/* Development Connection
+// Development Connection
 $con = mysqli_connect("localhost","root","","swe4633_grade_average");
-
-if(!$con){
-    die('Connection Failed'. mysqli_connect_error());
-}
-*/
-
-// Remote Connection
-
-$con = mysqli_connect("us-cdbr-east-06.cleardb.net","b6792ab368ddb3","bda03ad9","heroku_c43a872891278a3");
 
 if(!$con){
     die('Connection Failed'. mysqli_connect_error());
